8c7204f36e643ff46c225b5db80ddc3d6b9a5c38
Jan Kuepper <69371018+kupluspy@users.noreply.github.com>
2021-01-14 10:59:04 +0100

    Create sketch.ino

---
+++ sketch.ino
@@ -0,0 +1,94 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+HTTPClient sender;
+
+// WLAN-Daten
+const char* ssid = "WLAN_SSID";
+const char* password = "WLAN_PASSWD";
+
+//Messintervall in Sekunden
+int wait = 5;
+
+//Temperatur
+float temperature;
+
+
+void push(){
+
+  //Hier wird der Wert an die Smarthome-Umgebung übertragen
+  
+  if (sender.begin("http://SERVERIP:51828/?accessoryId=SENSORID&value=" + String(temperature))){
+
+    // HTTP-Code der Response speichern
+    int httpCode = sender.GET();
+   
+
+    if (httpCode > 0) {
+      
+      // Anfrage wurde gesendet und Server hat geantwortet
+      // Info: Der HTTP-Code für 'OK' ist 200
+      if (httpCode == HTTP_CODE_OK) {
+
+        // Hier wurden die Daten vom Server empfangen
+
+        // String vom Webseiteninhalt speichern
+        String payload = sender.getString();
+
+        // Hier kann mit dem Wert weitergearbeitet werden
+       	// ist aber nicht unbedingt notwendig
+        Serial.println(payload);
+        
+        
+        
+      }
+      
+    }else{
+      // Falls HTTP-Error
+      Serial.printf("HTTP-Error: ", sender.errorToString(httpCode).c_str());
+    }
+
+    // Wenn alles abgeschlossen ist, wird die Verbindung wieder beendet
+    sender.end();
+    
+  }else {
+    Serial.printf("HTTP-Verbindung konnte nicht hergestellt werden!");
+  }
+
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+
+  Serial.println("Verbunden!");
+  wait = wait * 1000;
+  sensors.begin();
+  
+}
+
+
+void loop() {
+  
+  sensors.requestTemperatures();
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.println(" °C");
+  
+  temperature = sensors.getTempCByIndex(0);
+  push();
+  delay(wait);
+
+}
